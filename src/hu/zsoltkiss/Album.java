a7fa245af9291ebeea04500c64b7ee1428f0444b
droidteacher <droid.oktato@gmail.com>
2025-04-17 00:51:12 +0200

    Implement Album

---
+++ src/hu/zsoltkiss/Album.java
@@ -0,0 +1,14 @@
+package hu.zsoltkiss;
+
+public class Album {
+
+    private String artist;
+    private String title;
+    private int year;
+
+    public Album(String artist, String title, int year) {
+        this.artist = artist;
+        this.title = title;
+        this.year = year;
+    }
+}
