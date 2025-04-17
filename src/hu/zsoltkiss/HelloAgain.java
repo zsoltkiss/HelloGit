f4f7da4e04d162e28775775a301966f416a807ba
Zsolt Kiss <kissrudolfzsolt@gmail.com>
2025-04-17 18:39:02 +0200

    Fix typo

--- src/hu/zsoltkiss/HelloAgain.java
+++ src/hu/zsoltkiss/HelloAgain.java
@@ -4,6 +4,6 @@ public class HelloAgain {
 
     public static void main(String[] args) {
         System.out.println("We are back in 2025");
-        System.out.println("Másik módosítás");
+        System.out.println("Egyéb igények");
     }
 }
