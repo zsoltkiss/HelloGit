a4b9d95072e86e309b885c6ba2205c4c575feeac
Zsolt Kiss <kissrudolfzsolt@gmail.com>
2025-04-17 18:25:36 +0200

    Modify print content

--- src/hu/zsoltkiss/HelloAgain.java
+++ src/hu/zsoltkiss/HelloAgain.java
@@ -4,5 +4,6 @@ public class HelloAgain {
 
     public static void main(String[] args) {
         System.out.println("We are back in 2025");
+        System.out.println("Másik módosítás");
     }
 }
